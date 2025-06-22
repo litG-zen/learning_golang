9ac603d4feb7fa4e6fc403530279d5f21c298a90
litG-zen <lalit.coder2296@gmail.com>
2025-06-22 09:06:48 +0530

    typo-fix

--- web_dev/auth/auth.go
+++ web_dev/auth/auth.go
@@ -8,7 +8,7 @@ import (
 
 const API_KEY = "f902b6d8-cfc9-4e62-84aa-1d5c44fe27ec"
 
-// ToDo : try adding JWT token based authentication for critical APIsgit
+// ToDo : try adding JWT token based authentication for critical APIs
 // 2025-06-22
 // Context-switched to JWT token creation after hotheading on logs ! phewww !
 func GenerateJWT(userID int) (string, error) {
