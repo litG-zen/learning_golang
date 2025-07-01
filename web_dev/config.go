fb2e5ca78759967b9d1cc56c603eaf93a2a8d99d
litG-zen <lalit.coder2296@gmail.com>
2025-07-01 06:36:30 +0530

    Random port initiation attempt learning

--- web_dev/config.go
+++ web_dev/config.go
@@ -1,7 +1,9 @@
 package main
 
 const (
-	PORT                = 8000
+	// PORT                = rand.IntN(65535) `This will not work as Golang returns a value error namely
+	//                                         [WARNING] You trusted all proxies, this is NOT safe. We recommend you to set a value.'
+	PORT                = 8000 // Adding server spinup on random for erytime, avoid attacks
 	INVALID_API_MSG     = "Invalid API KEY"
 	LOG_EXP_DELTA       = 86400 //24*60*60
 	JWT_TOKEN_EXP_DELTA = 86400 //24*60*60
