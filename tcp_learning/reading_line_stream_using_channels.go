af256d85a706e466403c1817ff6c8b9fdfa73939
litG-zen <lalit.coder2296@gmail.com>
2025-08-15 16:09:45 +0530

    TCP-Learning 1.2 - Reading stream of data , line-by-line using Go Channels and GoRoutines

---
+++ tcp_learning/reading_line_stream_using_channels.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			// Reading splitting at new-line.
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:n])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+		if len(str) != 0 {
+			out <- str
+		}
+
+	}()
+	return out
+}
+
+func main() {
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Println(line)
+	}
+}
