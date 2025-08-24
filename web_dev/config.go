607ae56c06c2589a6d6ace0c44b9fc40b2a2cc91
litG-zen <lalit.coder2296@gmail.com>
2025-08-24 22:54:34 +0530

    config update

--- web_dev/config.go
+++ web_dev/config.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	// PORT                = rand.IntN(65535) `This will not work as Golang returns a value error namely
+	// PORT             = rand.IntN(65535) `This will not work as Golang returns a value error namely
 	//                                         [WARNING] You trusted all proxies, this is NOT safe. We recommend you to set a value.'
 	PORT                = 8000 // Adding server spinup on random for erytime, avoid attacks
 	INVALID_API_MSG     = "Invalid API KEY"
